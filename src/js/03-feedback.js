030c027eb45159834dd915baf9eec5e7f9d4eddb
IrynaShashko <irishkashashko@gmail.com>
2022-05-28 21:53:07 +0300

    add 3 task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
+import throttle from "lodash.throttle";
+
+const STORAGE_KEY = "feedback-form-state";
+
+const refs = {
+    form: document.querySelector(".feedback-form"),
+    email: document.querySelector("input"),
+    message: document.querySelector("textarea"),
+};
+refs.form.addEventListener("input", throttle(formInput, 500));
+refs.form.addEventListener("submit", formSubmit);
+
+function formInput() {
+    const value = {
+        email: refs.email.value,
+        message: refs.message.value,
+    }
+    
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(value));
+}
+
+function formSubmit(e) {
+    e.preventDefault();
+    e.currentTarget.reset();
+    console.log(localStorage.getItem(STORAGE_KEY));
+    localStorage.removeItem(STORAGE_KEY);
+    
+}
+
+formOutput();
+
+function formOutput() {
+    const savedOutput = JSON.parse(localStorage.getItem(STORAGE_KEY));
+    if (savedOutput) {
+        refs.email.textContent = savedOutput.email;
+        refs.message.textContent = savedOutput.message;
+    }
+}
 
